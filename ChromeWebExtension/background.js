1c3e02d80dd5125b05cbe173af5a95f4b5429994
Yu Hsiang <umworksite@gmail.com>
2022-05-12 14:24:18 +0800

    #1 Add date input

--- ChromeWebExtension/background.js
+++ ChromeWebExtension/background.js
@@ -1,4 +1,30 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('setup')
+chrome.runtime.onInstalled.addListener(async() => {
+  await chrome.storage.sync.get("defaultTimeRange", ({ defaultTimeRange }) => {
+    if(defaultTimeRange){
+      chrome.storage.sync.set({ defaultTimeRange : defaultTimeRange });  
+      console.log(`Time range : `, defaultTimeRange);
+    }else{
+      const defaultTimeRange ={
+        start: {
+          year:'1995',
+          month:'10',
+          day:'10',
+        },
+        end: {
+          year: (new Date()).getFullYear(),
+          month: ((new Date()).getMonth()+1),
+          day:(new Date()).getDate(),
+        }
+      }
+      chrome.storage.sync.set({ defaultTimeRange : defaultTimeRange });
+    }
+  });
 });
 
+const input = document.getElementById('startDate');
+
+input.addEventListener('change', updateValue);
+
+function updateValue(e) {
+  log.textContent = e.target.value;
+}
--- ChromeWebExtension/content.js
+++ ChromeWebExtension/content.js
@@ -1,23 +1,14 @@
-function init(){
+async function init(){
   console.log("GoogleSearchTimeRange Load...")
   const queryString = window.location.search
   const urlParams = new URLSearchParams(queryString)
   const tbs = urlParams.get('tbs')
-  const timeRange ={
-    start: {
-      year:'1995',
-      month:'10',
-      day:'10',
-    },
-    end: {
-      year:'2000',
-      month:'10',
-      day:'10',
+  await chrome.storage.sync.get("defaultTimeRange", ({ defaultTimeRange }) => {
+    let timeRange = defaultTimeRange;
+    if(!tbs){
+      const directUrl = `${window.location.href}&tbs=cdr%3A1%2Ccd_min%3A${timeRange.start.month}%2F${timeRange.start.day}%2F${timeRange.start.year}%2Ccd_max%3A${timeRange.end.month}%2F${timeRange.end.day}%2F${timeRange.end.year}`
+      window.location.href = directUrl
     }
-  }
-  if(!tbs){
-    const directUrl = `${window.location.href}&tbs=cdr%3A1%2Ccd_min%3A${timeRange.start.month}%2F${timeRange.start.day}%2F${timeRange.start.year}%2Ccd_max%3A${timeRange.end.month}%2F${timeRange.end.day}%2F${timeRange.end.year}`
-    window.location.href = directUrl
-  }
+  });
 }
 init();
